f70a9ce1bec898aedf1c85d6c956bb57a251b3a4
Lisa Walters <lisacampiwalters@gmail.com>
2023-06-18 22:28:35 -0600

    created index.cshtml file

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Testing
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Products>GetAllProducts();
+    }
+}
---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using Dapper;
+using System.ComponentModel.Design;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn; 
+        public ProductRepository(IDbConnection conn) { 
+            _conn = conn;
+        
+        
+        }
+
+        public IEnumerable<Products> GetAllProducts()
+        {
+            return _conn.Query<Products>("SELECT * FROM Products");
+        }
+    }
+}

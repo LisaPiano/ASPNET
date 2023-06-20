69ec43707a30a71826145c3a648415e436b9d275
Lisa Walters <lisacampiwalters@gmail.com>
2023-06-19 18:21:13 -0600

    Completed 3rd section and added UpdateProduct

--- Testing/ProductRepository.cs
+++ Testing/ProductRepository.cs
@@ -25,6 +25,12 @@ namespace Testing
         public Product GetProduct(int id)
         {
             return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE ProductId = @id", new { id = id });
-        } 
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
+            new { name = product.Name, price = product.Price, id = product.ProductId });
+        }
     }
 }
